c49ca8a31c753e0e013b53502519c519aa51f6f2
rthawkins <48301451+rthawkins@users.noreply.github.com>
2019-09-26 21:24:39 -0500

    box office data fix

--- df.py
+++ df.py
@@ -27,7 +27,23 @@ genre = []
 box_office = []
 year = []
 
-record = 1
+notfound = 0
+found = 0
+total = len(df)
+j = 0
+
+films = df["Title"]
+titles = []
+metascore = []
+imdb_rating = []
+imdb_votes = []
+rated = []
+runtime = []
+released = []
+genre = []
+box_office = []
+year = []
+
 notfound = 0
 found = 0
 total = len(df)
@@ -36,6 +52,7 @@ j = 0
 for film in films:
     try:
         response = omdb.get(title={film})
+        box_office.append(response["box_office"])
         titles.append(response["title"])
         rated.append(response["rated"])
         genre.append(response["genre"])
@@ -48,14 +65,11 @@ for film in films:
         released.append(response["released"])
         year.append(response["year"])
 
-        box_office.append(response["box_office"])
         j += 1
-        record += 1
         found += 1
         print(f"Processing Record {j} of {total} | {film}")
     except Exception:
         print(f"Processing Record {j} of {total} | {film} not found - skipping.")
-        record += 1
         notfound += 1
         
 pct_movie_found = round(found/(notfound+found)*100,1)       
@@ -63,6 +77,6 @@ print(f"{pct_movie_found}% of movies found")
 
 # Coverting to a dataframe 
 
-df = pd.DataFrame({'Title': titles, 'Metascore': metascore, 'Rated': rated,'Runtime': runtime,'Released': released, 'Year': year, 'Genre': genre,
+df = pd.DataFrame({'Title': titles, 'Metascore': metascore, 'Rated': rated,'Runtime': runtime,'Released': released, 'Year': year, 'Genre': genre,'Box Office': box_office,
                   'imdb_rating': imdb_rating, 'imdb_votes': imdb_votes})
 df.to_csv("films.csv", encoding="utf-8", index=False)
